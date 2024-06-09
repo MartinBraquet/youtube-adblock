b8888e87764eb8700a4cfb5153cdac26c73a39d4
MartinBraquet <martin.braquet@gmail.com>
2024-06-08 20:53:10 -0500

    Test

--- tag.sh
+++ tag.sh
@@ -7,6 +7,12 @@ if [ -z "$tagged" ]; then
   git tag -a $tag -m "Release $tag"
   git push origin $tag
   echo "Tagged release $tag"
+
+  gh release create "$tag" \
+      --repo="$GITHUB_REPOSITORY" \
+      --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
+      --generate-notes
+  echo "Created release"
 else
   echo "Tag $tag already exists"
 fi
