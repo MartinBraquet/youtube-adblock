480db1d3430ebef755f76d020b6a81eddc9775ba
MartinBraquet <martin.braquet@gmail.com>
2024-06-08 20:28:07 -0500

    Tag

--- release.sh
+++ release.sh
@@ -2,7 +2,9 @@ tag=$(python -c "import json; print(json.load(open('manifest.json'))['version'])
 echo "Tagging release $tag"
 tagged=$(git tag -l $tag)
 if [ -z "$tagged" ]; then
-  export TAG=$tag
+  git tag -a $tag -m "Release $tag"
+  git push origin $tag
+  echo "Tagged release $tag"
 else
   echo "Tag $tag already exists"
 fi
