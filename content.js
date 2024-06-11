fe093f654939396b6bfc72a6e5a41159ea9be66a
MartinBraquet <martin.braquet@gmail.com>
2024-06-11 01:37:31 -0500

    Fix log

--- content.js
+++ content.js
@@ -26,7 +26,7 @@ function isAdPersistentBar(element) {
 }
 
 function adExist(element) {
-    console.log("isAdPersistentBar", isAdPersistentBar(element));
+    // console.log("isAdPersistentBar", isAdPersistentBar(element));
     return element.getElementsByClassName("ad-showing").length > 0 || isAdPersistentBar(element);
 }
 
