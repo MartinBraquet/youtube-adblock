e420f9aae75c5f7d11c7d5c8234cf31f95e86f63
MartinBraquet <martin.braquet@gmail.com>
2024-06-12 10:41:08 -0500

    Make boosting playback rate optional (default: No)

--- popup.js
+++ popup.js
@@ -2,10 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     browser.storage.local.get('adsSkipped', (data) => {
         document.getElementById('adsSkippedCount').textContent = data.adsSkipped || '0';
     });
+
     browser.storage.local.get('adPlaybackRate', (data) => {
         document.getElementById('adPlaybackRate').value = data.adPlaybackRate || 5;
     });
     document.getElementById('adPlaybackRate').addEventListener('change', (event) => {
         browser.storage.local.set({adPlaybackRate: event.target.value});
+    });
+
+    browser.storage.local.get('boostWanted', (data) => {
+        document.getElementById('boostWanted').checked = data.boostWanted;
+    });
+    document.getElementById('boostWanted').addEventListener('change', (event) => {
+        browser.storage.local.set({boostWanted: event.target.checked});
     });
 });
